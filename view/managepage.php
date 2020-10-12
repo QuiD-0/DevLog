3d726bc00163710361e97f3e9a425204942e5b08
QuiD <wodnd101@naver.com>
2020-10-12 21:44:53 +0900

    Update managepage.php

--- view/managepage.php
+++ view/managepage.php
@@ -28,7 +28,13 @@
             <li><a href="managepage.php?id=3">Delete</a></li>
             <div class="user">
                 <!--로그인 정보 출력-->
-                <?php include("../php/topSession.php");?></div>
+                <?php $user_id = $_SESSION['user_id'];
+                $user_name = "QuiD";
+                //로그인된 유저 닉네임 표시
+                echo "<p><i class=\"far fa-user\"></i>{$user_name}님";
+                //로그아웃 버튼
+                echo "<a href=\"/php/logout.php\" class=\"logout\"><i class=\"fas fa-sign-out-alt\"></i>logout</a></p>";
+?></div>
         </ul>
     <div class="main">
         <div class="search">
